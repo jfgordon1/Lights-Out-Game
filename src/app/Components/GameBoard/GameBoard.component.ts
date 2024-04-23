a39fa6490f633d450b9126df9299cfd64f854831
Jacob Gordon <jfgordon@udel.edu>
2024-04-23 14:42:42 -0400

    cleaned up buttons so that its just the buttons that appear

--- src/app/Components/GameBoard/GameBoard.component.ts
+++ src/app/Components/GameBoard/GameBoard.component.ts
@@ -7,13 +7,21 @@ import { BoardButtonRowComponent } from "../boardButtonRow/boardButtonRow.compon
 export class GameBoardComponent extends EzComponent {
     private rows: BoardButtonRowComponent[] = [];
 
+    length = 5;
+
     constructor() {
         super(html, css);
     }
 
     @Click("makeBoard")
     onMakeBoard() {
-        for (let i = 0; i < 5; i++) {
+        if (this.rows.length > 0) {
+            for (let row of this.rows) {
+                this.removeComponent(row);
+            }
+        }
+        this.rows = [];
+        for (let i = 0; i < this.length; i++) {
             const row = new BoardButtonRowComponent();
             this.addComponent(row);
             this.rows.push(row);
