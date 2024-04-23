d57d9a3f8ae3760b516ad1070a645a937d245e5e
Jacob Gordon <jfgordon@udel.edu>
2024-04-23 14:03:22 -0400

    added boardButtonRows to be displayed. Need to add buttons to row components

--- src/app/Components/GameBoard/GameBoard.component.ts
+++ src/app/Components/GameBoard/GameBoard.component.ts
@@ -1,15 +1,22 @@
-import { EzComponent } from "@gsilber/webez";
+import { Click, EzComponent } from "@gsilber/webez";
 import html from "./GameBoard.component.html";
 import css from "./GameBoard.component.css";
+import { BoardButtonRowComponent } from "../boardButtonRow/boardButtonRow.component";
 //import { BoardButtonComponent } from "../boardButton/boardButton.component";
 
 export class GameBoardComponent extends EzComponent {
-    //private layout: BoardButtonComponent;
+    private rows: BoardButtonRowComponent[] = [];
+
     constructor() {
         super(html, css);
     }
 
-    getHeight() {}
-
-    getWidth() {}
+    @Click("makeBoard")
+    onMakeBoard() {
+        for (let i = 0; i < 5; i++) {
+            const row = new BoardButtonRowComponent();
+            this.addComponent(row);
+            this.rows.push(row);
+        }
+    }
 }
--- src/app/Components/boardButtonRow/boardButtonRow.component.ts
+++ src/app/Components/boardButtonRow/boardButtonRow.component.ts
@@ -1,9 +1,16 @@
 import { EzComponent } from "@gsilber/webez";
 import html from "./boardButtonRow.component.html";
 import css from "./boardButtonRow.component.css";
+import { BoardButtonComponent } from "../boardButton/boardButton.component";
 
 export class BoardButtonRowComponent extends EzComponent {
+    row: BoardButtonComponent[] = [];
     constructor() {
         super(html, css);
+        for (let i = 0; i < 5; i++) {
+            const temp = new BoardButtonComponent();
+            this.addComponent(temp, "buttons");
+            this.row.push(temp);
+        }
     }
 }
