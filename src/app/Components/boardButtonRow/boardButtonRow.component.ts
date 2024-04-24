83651874e64afc706cb3a3827f227979e7f1b304
Jacob Gordon <jfgordon@udel.edu>
2024-04-24 00:07:45 -0400

    added a length attribute to buttonRowComponent

--- src/app/Components/boardButtonRow/boardButtonRow.component.ts
+++ src/app/Components/boardButtonRow/boardButtonRow.component.ts
@@ -5,9 +5,10 @@ import { BoardButtonComponent } from "../boardButton/boardButton.component";
 
 export class BoardButtonRowComponent extends EzComponent {
     row: BoardButtonComponent[] = [];
+    length: number = 3;
     constructor() {
         super(html, css);
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < this.length; i++) {
             const temp = new BoardButtonComponent();
             this.addComponent(temp, "buttons");
             this.row.push(temp);
