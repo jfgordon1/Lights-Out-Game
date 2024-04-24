179064ee0a05cedf55d6dd733861ea9d3a348fd8
Jacob Gordon <jfgordon@udel.edu>
2024-04-23 20:29:07 -0400

    got button css working

--- src/app/Components/boardButton/boardButton.component.ts
+++ src/app/Components/boardButton/boardButton.component.ts
@@ -1,9 +1,24 @@
-import { EzComponent } from "@gsilber/webez";
+import { BindCSSClassToBoolean, Click, EzComponent } from "@gsilber/webez";
 import html from "./boardButton.component.html";
 import css from "./boardButton.component.css";
 
 export class BoardButtonComponent extends EzComponent {
+    private onOrOff = false;
     constructor() {
         super(html, css);
+    }
+
+    @Click("button")
+    onClicked() {
+        this.changeColor();
+    }
+
+    @BindCSSClassToBoolean("button", "button-off") turnItOff: boolean = true;
+    changeColor() {
+        if (this.turnItOff) {
+            this.onOrOff = false;
+        } else {
+            this.onOrOff = true;
+        }
     }
 }
