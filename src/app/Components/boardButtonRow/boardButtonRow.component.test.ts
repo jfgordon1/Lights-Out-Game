eda423505f12a6da7ad135d9e64c86f2fe2585de
Jacob Gordon <jfgordon@udel.edu>
2024-04-30 13:40:05 -0400

    added tests for BoardButtonRow

--- src/app/Components/boardButtonRow/boardButtonRow.component.test.ts
+++ src/app/Components/boardButtonRow/boardButtonRow.component.test.ts
@@ -6,11 +6,34 @@ describe("BoardButtonRowComponent", () => {
     let component: any = undefined;
     beforeAll(() => {
         const html: string = `<div>Testing Environment</div><div id='main-target'></div>`;
-        component = bootstrap<BoardButtonRowComponent>(BoardButtonRowComponent, html);
+        component = bootstrap<BoardButtonRowComponent>(
+            BoardButtonRowComponent,
+            html,
+        );
     });
     describe("Constructor", () => {
         test("Create Instance", () => {
             expect(component).toBeInstanceOf(BoardButtonRowComponent);
+        });
+    });
+    describe("Check Row Colors", () => {
+        test("all off", () => {
+            const row = new BoardButtonRowComponent(3);
+            for (let i = 0; i < 3; i++) {
+                if (row.row[i].checkColor()) {
+                    row.row[i].changeColor();
+                }
+            }
+            expect(row.checkOff).toBeTruthy();
+        });
+        test("all on", () => {
+            const row = new BoardButtonRowComponent(3);
+            for (let i = 0; i < 3; i++) {
+                if (row.row[i].checkColor() === "gray") {
+                    row.row[i].changeColor();
+                }
+            }
+            expect(row.checkOff()).toBeFalsy();
         });
     });
 });
